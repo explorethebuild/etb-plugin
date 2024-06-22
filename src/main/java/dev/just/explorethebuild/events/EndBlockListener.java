8b7804f7a0224a7b6a9b31cc281adfad97e72fa1
Justus <justus@justcoding.tech>
2024-06-22 14:52:20 +0200

    Use alternative event for detection of entering the end dimension

--- src/main/java/dev/just/explorethebuild/events/EndBlockListener.java
+++ src/main/java/dev/just/explorethebuild/events/EndBlockListener.java
@@ -6,14 +6,25 @@ import dev.just.explorethebuild.utils.Config;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 
 public class EndBlockListener implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         if (event.getTo().getWorld().getName().contains("the_end")) {
             if (!Config.getBoolean("isEndAllowed", false)) {
-                event.setCancelled(true);
+                event.getPlayer().sendMessage(Main.getErrorPrefix() + "Da das End-Event noch nicht gestartet ist, " +
+                        "wurdest du an den Spawn zurückgesetzt.");
                 event.getPlayer().teleport(ElytraSystem.getElytraSpawnPosition());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerPortal(PlayerPortalEvent event) {
+        if (event.getTo().getWorld().getName().contains("the_end")) {
+            if (!Config.getBoolean("isEndAllowed", false)) {
+                event.setTo(ElytraSystem.getElytraSpawnPosition());
                 event.getPlayer().sendMessage(Main.getErrorPrefix() + "Da das End-Event noch nicht gestartet ist, " +
                         "wurdest du an den Spawn zurückgesetzt.");
             }
